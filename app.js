eb529541b2b84202c98574c9c472b573c0cc6cd8
Alexnoch <44502017+Alexnoch@users.noreply.github.com>
2020-10-29 01:38:16 +0300

    aasd

--- app.js
+++ app.js
@@ -37,7 +37,7 @@
 const http = require('http');
 
 http.createServer((req,res)=>{
-    res.writeHead({"Access-Control-Allow-Origin":"*"});
+    res.writeHead({"Access-Control-Allow-Origin:http://alexnoch-blog.ru"});
     res.writeHead(200, {
         "Content-Type": "application/json"
     });
